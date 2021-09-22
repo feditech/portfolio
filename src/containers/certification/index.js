8529f5a324c94089b6ec1c6e98425345a4ffc8ea
fahad <feditechnologies@gmail.com>
2021-09-23 04:30:36 +0500

    expertise and certification page done

--- src/containers/certification/index.js
+++ src/containers/certification/index.js
@@ -1,9 +1,18 @@
-import {Header} from '../../components'
-function Certification(){
-    return(
+import { Header } from '../../components'
+import './css/style.css'
+function Certification() {
+    return (
         <div>
             <Header />
-            Certification
+            <div className='certificationDiv'>
+                <h1>Certification</h1>
+                <ul>
+                    <li><h2>Microsoft HTML CSS</h2> </li>
+                    <li><h2>Microsoft JS (Pending)</h2> </li>
+                    <li><h2>Microsoft React JS (Pending)</h2> </li>
+                </ul>
+            </div>
+
         </div>
     )
 }
--- src/containers/expertise/index.js
+++ src/containers/expertise/index.js
@@ -1,10 +1,21 @@
 import {Header} from '../../components'
+import './css/style.css'
 function Expertise(){
 
     return(
         <div>
             <Header />
-            Expertise
+            <div className="expertiseDiv">
+                <h1>Expertise</h1>
+            <ul> 
+            <li><h2>Html</h2> </li>
+            <li><h2>Css</h2> </li>
+            <li><h2>Javascript</h2> </li>
+            <li><h2>React</h2> </li>
+            </ul>
+            
+            
+            </div>
             
         </div>
     )
