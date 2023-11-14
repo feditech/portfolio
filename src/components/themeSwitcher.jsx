4b1cf4af5def6cb338455a7dbf6dd02916610a4d
fahad <fahdforr@gmail.com>
2023-11-15 02:43:34 +0500

    theme switcher button done

---
+++ src/components/themeSwitcher.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import sun from '../assets/icons/sun.svg';
+import moon from '../assets/icons/moon.svg';
+
+const ThemeSwitcher = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
+    return (
+        <div className={`toggle cursor-pointer relative w-14 pt-[2px] h-[32px]  border-2 border-black rounded-3xl flex  items-center `} onClick={toggleDarkMode}>
+            {isDarkMode
+                ?
+                <div
+                    className={`icon-container absolute  left-0 transform ${isDarkMode ? 'translate-x-0' : 'translate-x-full'
+                        } transition-transform duration-1000`}
+                >
+                    <img src={sun} alt="sun" className="w-7 h-7" />
+                </div>
+                :
+                <div
+                    className={`icon-container absolute  right-0 transform ${isDarkMode ? 'translate-x-full' : 'translate-x-0'
+                        } transition-transform duration-1000`}
+                >
+                    <img src={moon} alt="moon" className="w-7 h-7" />
+                </div>
+            }
+        </div>
+    );
+};
+
+export default ThemeSwitcher;
